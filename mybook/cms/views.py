1f6f1262cc71fdee23c1b7eb6334f31e183d361f
OMORI <r.shimizu18020@gmail.com>
2021-01-24 23:21:21 +0900

    Add Bootstrap and jQuery.

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def book_list(request):
+    return HttpResponse('List of Book info')
+
+
+def book_edit(request, book_id=None):
+    return HttpResponse('Edit Book info')
+
+
+def book_del(request, book_id):
+    return HttpResponse('Delete Book')
