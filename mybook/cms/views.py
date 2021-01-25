102f5780b415b0d31a3bafedd88039bf2ef920ad
OMORI <r.shimizu18020@gmail.com>
2021-01-25 23:43:54 +0900

    Add some view.

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -7,8 +7,24 @@ def book_list(request):
 
 
 def book_edit(request, book_id=None):
-    return HttpResponse('Edit Book info')
+    if book_id:
+        book = get_object_or_404(Book, pk=book_id)
+    else:
+        book = Book()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:
+        form = BookForm(instance=book)
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 
 def book_del(request, book_id):
-    return HttpResponse('Delete Book')
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
